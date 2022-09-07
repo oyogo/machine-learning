2b70d6e092dbb8fb900239cb9ec5846ee39ba503
oyogo <oyogoc@gmail.com>
2022-09-07 19:49:20 +0300

    done fitting the model, yielded an 89 percent accuracy.

--- Classification/SupportVectorMachine.py
+++ Classification/SupportVectorMachine.py
@@ -4,6 +4,9 @@
 # Load the libraries 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 # Import the data
 svc_data = pd.read_csv("../data/student_performance.csv")
@@ -16,7 +19,7 @@ svc_data.info()
 
 svc_data = svc_data.replace({'address':{'R':0,'U':1},
                             'famsize':{'LE3':0,'GT3':1},
-                            'Pstatus':{'P':0,'A':1},
+                            'Pstatus':{'T':0,'A':1},
                             'Mjob':{'teacher':0,'at_home':1,'services':2,'other':3,'health':4},
                             'Fjob':{'teacher':0,'at_home':1,'services':2,'other':3,'health':4},
                             'guardian':{'mother':0,'father':1,'other':2},
@@ -39,5 +42,23 @@ svc_data['G3'] = np.where(svc_data['G3']>=10,1,0)
 
 # Some columns are not really needed as such we'll drop them. 
 
-svc_data.head()                            
+svc_data.drop(['G1','reason','school'], axis=1,inplace=True)
 
+# Data partitioning 
+## response variable
+y = svc_data['G3']
+## predictor variables
+X = svc_data.drop('G3',axis=1)
+
+# Split the data into training and testing sets
+x_train,x_test,y_train,y_test = train_test_split(X,y, test_size = 0.2) 
+
+## Fit the model 
+svcclassifier = SVC(kernel='linear')
+svcclassifier.fit(x_train,y_train)
+
+ # Make predictions on the test data                         
+y_pred = svcclassifier.predict(x_test)
+
+# Use the accuracy_score method from sklearn metric module to calculate the accuracy.
+print("Accuracy: ", accuracy_score(y_test,y_pred))
